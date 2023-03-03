7bd101a4f02842fe9f845304ace70df799dd6187
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-03-03 10:10:56 +0300

    add toasty

---
+++ src/helper/Toasty.js
@@ -0,0 +1,27 @@
+import { toast } from "react-toastify";
+
+export const toastSuccessNotify = (msg) => {
+  toast.success(msg, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const toastErrorNotify = (msg) => {
+  toast.error(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
